77ccf26271b13a440f5d1fa07026d504d807c6a8
DQQ <n7xh92x37k97b@163.com>
2026-04-22 20:11:34 +0800

    添加了商品对应的图片、购物车商品的结算功能

--- mshop/pages/category/category.js
+++ mshop/pages/category/category.js
@@ -11,16 +11,16 @@ Page({
       { id: 5, name: "AirPods" }
     ],
     allGoods: [
-      { id: 1, name: "iPhone 17 Pro", price: 7999, image: "https://img.icons8.com/400/iphone-17-pro.png", cid: 1 },
-      { id: 2, name: "iPhone 17", price: 5999, image: "https://img.icons8.com/400/iphone-17.png", cid: 1 },
-      { id: 3, name: "MacBook Air M3", price: 8999, image: "https://img.icons8.com/400/macbook-air.png", cid: 2 },
-      { id: 4, name: "MacBook Pro 14", price: 12999, image: "https://img.icons8.com/400/macbook-pro.png", cid: 2 },
-      { id: 5, name: "iPad Pro M4", price: 6799, image: "https://img.icons8.com/400/ipad-pro.png", cid: 3 },
-      { id: 6, name: "iPad Air", price: 4599, image: "https://img.icons8.com/400/ipad-air.png", cid: 3 },
-      { id: 7, name: "Apple Watch S10", price: 3199, image: "https://img.icons8.com/400/apple-watch.png", cid: 4 },
-      { id: 8, name: "Watch SE", price: 1999, image: "https://img.icons8.com/400/apple-watch-se.png", cid: 4 },
-      { id: 9, name: "AirPods Pro 3", price: 1899, image: "https://img.icons8.com/400/airpods-pro.png", cid: 5 },
-      { id: 10, name: "AirPods 4", price: 999, image: "https://img.icons8.com/400/airpods.png", cid: 5 }
+      { id: 1, name: "iPhone 17 Pro", price: 7999, image: "/images/tab/iPhone_17_Pro.jpg", cid: 1 },
+      { id: 2, name: "iPhone 17", price: 5999, image: "/images/tab/iPhone17.jpg", cid: 1 },
+      { id: 3, name: "MacBook Air M3", price: 8999, image: "/images/tab/MacBook_Air_M3.jpg", cid: 2 },
+      { id: 4, name: "MacBook Pro 14", price: 12999, image: "/images/tab/MacBook_Pro_14.jpg", cid: 2 },
+      { id: 5, name: "iPad Pro M4", price: 6799, image: "/images/tab/iPad_Pro_M4.jpg", cid: 3 },
+      { id: 6, name: "iPad Air", price: 4599, image: "/images/tab/iPad_Air.jpg", cid: 3 },
+      { id: 7, name: "Apple Watch S10", price: 1999, image: "/images/tab/Apple_Watch_S10.jpg", cid: 4 },
+      { id: 8, name: "Watch SE3", price: 1299, image: "/images/tab/Watch_SE3.jpg", cid: 4 },
+      { id: 9, name: "AirPods Pro 3", price: 1899, image: "/images/tab/AirPods_Pro3.jpg", cid: 5 },
+      { id: 10, name: "AirPods 4", price: 999, image: "/images/tab/AirPods4.jpg", cid: 5 }
     ],
     goodsList: []
   },
